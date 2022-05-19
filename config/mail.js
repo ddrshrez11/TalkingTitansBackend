c7aa31bebf61541f7cf589a89f83ec8969b9739f
Yunip Shrestha <yunipshrestha@gmail.com>
2022-05-19 06:33:07 +0545

    Change to email sent to participants

--- config/mail.js
+++ config/mail.js
@@ -8,7 +8,7 @@ const contactEmail = {
     pass: process.env.SMTP_TO_PASSWORD,
   },
 };
-const toMail = process.env.MAIL_TO
+const toMail = process.env.MAIL_TO;
 const sendNewUserNotificationEmail = (newData) => {
   let transporter = nodemailer.createTransport(contactEmail);
 
@@ -19,7 +19,7 @@ const sendNewUserNotificationEmail = (newData) => {
     text: "Test Email",
     html: `
     <ul>
-        <li>name: ${newData.p_id}</li>
+        <li>pId: ${newData.pId}</li>
         <li>name: ${newData.name}</li>
         <li>address: ${newData.address}</li>
         <li>phone: ${newData.phone}</li>
@@ -49,18 +49,28 @@ const sendTokenNumEmail = (newData) => {
     to: newData.email,
     subject: "Talking Titans 2 - Registration Token",
     text: `Thank you for your participation!
-    Your token number for Khalti Talking Titans 2 is TT2-${newData.p_id}.
-    (To confirm your registration, please make sure you have paid the registration fee and updated your Khalti KYC.)
+    Your token number for Khalti Talking Titans 2 is TT2-${newData.pId}.
+
+    You need to compulsorily bring your Citizenship for verification on the mentorship day.
+
+    To confirm your registration, please make sure you have paid the registration fee. You can make the payment online in Khalti ID: 9818855727 (either from Khalti app or through fonepay). For offline payment, you can visit our registration office at Dursikshya Education Network, Kantipath (Opposite of Naach Ghar), Kathmandu.
+
+    And to receive the NPR.100/- Khalti cash back on your registration fee, get your Khalti cash back coupon on the mentorship day.
     
-    For any queries, contact 9808723759 | 9818855727 | 9860132809`,
+    For any queries, call/Whatsapp: 9808723759 | 9818855727 | 9860132809
+    Follow us to get all the Event Updates: https://www.facebook.com/shardaproduction
+    `,
     html: `
     <div style="font-size: 20px;">
     <h2>Thank you for your participation!</h2>
-    Your token number for Khalti Talking Titans 2 is <h1>TT2-${newData.p_id}.</h1>
-<em>(To confirm your registration, please make sure you have paid the registration fee and updated your Khalti KYC.)</em>
-<br />
-
-For any queries, contact 9808723759 | 9818855727 | 9860132809
+    Your token number for Khalti Talking Titans 2 is <h1>TT2-${newData.pId}.</h1>
+    <p><strong>You need to compulsorily bring your Citizenship for verification on the mentorship day.</strong></p>
+    <p>
+      To confirm your registration, please make sure you have paid the registration fee. You can make the payment online in Khalti ID: 9818855727 (either from Khalti app or through fonepay). For offline payment, you can visit our registration office at Dursikshya Education Network, Kantipath (Opposite of Naach Ghar), Kathmandu. <br />
+      & to receive the NPR.100/- Khalti Cash Back on your registration fee, get your Khalti Cash Back coupon on the mentorship day.
+    </p>
+    <p>For any queries, Call or Whatsapp:  9808723759 | 9818855727 | 9860132809 </p>
+    <p>Follow us to get all the Event Updates: <a href="https://www.facebook.com/shardaproduction">Facebook Page</a> </p>
 </div>`,
   };
 
